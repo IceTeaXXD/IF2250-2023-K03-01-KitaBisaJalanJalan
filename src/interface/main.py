b21f7550e360fe9c69f027e1c01ac2e4d36112ab
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-04-14 12:44:09 +0700

    refactor: class main

--- src/interface/main.py
+++ src/interface/main.py
@@ -1,11 +1,24 @@
-# GUI Main file
-from home import*
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import *
 
-app = QApplication(sys.argv)
-window = MainWindow()
-widget = QtWidgets.QStackedWidget()
-widget.addWidget(window)
-widget.setFixedWidth(1512)
-widget.setFixedHeight(982)
-widget.show()
-sys.exit(app.exec_())
+import home as home
+
+class MainApplication(QApplication):
+    def __init__(self, argv):
+        super(MainApplication, self).__init__(argv)
+        self.home = home.HomeWindow()
+        self.widget = QtWidgets.QStackedWidget()
+        self.widget.addWidget(self.home)
+        self.widget.setFixedWidth(1512)
+        self.widget.setFixedHeight(982)
+        self.widget.show()
+        self.home.button_baru.clicked.connect(self.button_baru_clicked)
+
+    def button_baru_clicked(self):
+        print("button baru clicked")
+
+run = MainApplication(sys.argv)
+sys.exit(run.exec_())
