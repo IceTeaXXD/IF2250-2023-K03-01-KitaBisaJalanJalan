2f629cc066a4ee8dfdf7872f7eff0597b6c9ccb7
MHEN2606 <matthew.mahendra@gmail.com>
2023-04-15 00:07:21 +0700

    feat: Integrate with Class BoundaryDaerahWisata

--- src/interface/pilihDaerah.py
+++ src/interface/pilihDaerah.py
@@ -3,9 +3,13 @@ from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import *
+from classes import BoundaryDaerahWisata
 
 class pilihDaerahWindow(QDialog):
     def __init__(self):
         super(pilihDaerahWindow, self).__init__()
-        loadUi("src/interface/ui/pilihDaerah.ui", self)
+        loadUi("./interface/ui/pilihDaerah.ui", self)
+        daerahwisata = BoundaryDaerahWisata()
+        for d in daerahwisata.getListDaerah():
+            self.dropdown.addItem(d.getNama(), d.getID())
         self.show()
