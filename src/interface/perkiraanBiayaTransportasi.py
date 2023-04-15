86eeb21a63bfcab766e4549bdc064868346a01fe
henryanandsr <13521004@std.stei.itb.ac.id>
2023-04-15 22:46:27 +0700

    feat : add perkiraan Biaya Transportasi

--- src/interface/perkiraanBiayaTransportasi.py
+++ src/interface/perkiraanBiayaTransportasi.py
@@ -74,17 +74,25 @@ class PerkiraanBiayaTransportasiWindow(QDialog):
             line.setStyleSheet("background-color: #000000; margin:10; padding: 0;")
             line.setFixedSize(380, 1)
 
-            desc = QLabel("lorem ipsum dolor sir amet")
+            scroll_area = QScrollArea()
+            scroll_area.setWidgetResizable(True)
+            scroll_area.setFixedHeight(100)
+            scroll_area.setFrameShape(QtWidgets.QFrame.NoFrame)
+            scroll_area.setStyleSheet("background-color: transparent;")
+
+            desc = QLabel("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
             desc.setStyleSheet("color: #000000; font-size: 16px;")
             desc.setWordWrap(True)
-            desc.setContentsMargins(10, 0, 0, 0)
+            desc.setMaximumHeight(600)
+            desc.setContentsMargins(10, 0, 10, 10)
+            scroll_area.setWidget(desc)
 
 
             left_box_layout = QVBoxLayout(left_box)
             left_box_layout.addWidget(decoration)
             left_box_layout.addWidget(title)
             left_box_layout.addWidget(line)
-            left_box_layout.addWidget(desc)
+            left_box_layout.addWidget(scroll_area)
             left_box_layout.setContentsMargins(0, 0, 0, 0)
 
             box.addWidget(left_box)
@@ -92,148 +100,106 @@ class PerkiraanBiayaTransportasiWindow(QDialog):
             title.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
             line.setContentsMargins(0, 0, 0, 0)
             desc.setAlignment(Qt.AlignTop | Qt.AlignLeft)
-    
+
             decoration.setMargin(0)
             right_box = QLabel()
             right_box.setFixedSize(800, 400)
             right_box.setStyleSheet("background-color: #F2F2F2; border-radius: 10px;")
-            right_box_layout = QVBoxLayout(right_box)
+            right_box.setAlignment(Qt.AlignCenter)
+            right_box_layout = QHBoxLayout(right_box)
             right_box_layout.setContentsMargins(0, 0, 0, 0)
-            # right_box.setAlignment(Qt.AlignCenter)
+            right_box_layout.setSpacing(0)
+            right_box.setAlignment(Qt.AlignCenter)
 
-            tiket_kereta = QLabel()
-            tiket_kereta.setContentsMargins(0, 0, 0, 0)
-            tiket_kereta.setMaximumWidth(600)
-            tiket_kereta.setMaximumHeight(200)
-
-            label_kereta = QLabel("Tiket Kereta Rp. " + str(arrray_of_destiansi[i][1]))
-            label_kereta.setStyleSheet("color: #000000; font-size: 32px; font-weight: bold; margin-bottom : 20px; margin-top : 20px; ")
-            # set coordinate
-            label_kereta.setContentsMargins(0, 0, 0, 0)
+            # radio button container
+            radio_container = QLabel()
+            # radio_container.setStyleSheet("margin-right  : 100")
+            # radio_container.setFixedSize(1000, 200)
+            radio_container.setFixedWidth(80)
+            # set spacing
             
-            # add an image
-            img_kereta = QLabel()
-            img_kereta.setStyleSheet("background-color: #000000; border-radius: 10px; margin-bottom : 20px")
-            img_kereta.setPixmap(QPixmap("D:/Semester IV/RPL/2/if2250-2023-k03-01-kitabisajalanjalan/img/biayaTransport/Train.png"))
-            img_kereta.setStyleSheet("background-color : transparent;")
-            img_kereta.setMaximumWidth(50)
-            # img_kereta.setMaximumHeight(100)
-
-            # define a button group
-            group = QButtonGroup()
+            
             # make a radio button
             radio_kereta = QRadioButton()
-            group.addButton(radio_kereta)
-            radio_kereta.setStyleSheet("background-color : transparent; margin-top: 20px; margin-left : 20px")
-
-            # add to layout
-            layout_kereta = QHBoxLayout(tiket_kereta)
-            layout_kereta.setContentsMargins(0, 0, 0, 0)
-            layout_kereta.addWidget(radio_kereta)
-            layout_kereta.addWidget(img_kereta)
-            layout_kereta.addWidget(label_kereta)
-            layout_kereta.setAlignment(Qt.AlignCenter)
-
-
-            tiket_mobil = QLabel()
-            tiket_mobil.setContentsMargins(0, 0, 0, 0)
-            tiket_mobil.setMaximumWidth(600)
-            tiket_mobil.setMaximumHeight(200)
-
-            label_mobil = QLabel("Tiket Mobil Rp. " + str(arrray_of_destiansi[i][2]))
-            label_mobil.setStyleSheet("color: #000000; font-size: 32px; font-weight: bold; margin-bottom : 20px; margin-top : 20px; ")
-            # set coordinate
-            label_mobil.setContentsMargins(0, 0, 0, 0)
-
-            # add an image
-            img_mobil = QLabel()
-            img_mobil.setStyleSheet("background-color: #000000; border-radius: 10px; margin-bottom : 20px")
-            img_mobil.setPixmap(QPixmap("D:/Semester IV/RPL/2/if2250-2023-k03-01-kitabisajalanjalan/img/biayaTransport/People in Car.png"))
-            img_mobil.setStyleSheet("background-color : transparent;")
-            img_mobil.setMaximumWidth(70)
-
-            # make a radio button
-            radio_mobil = QRadioButton()
-            group.addButton(radio_mobil)
-            radio_mobil.setStyleSheet("background-color : transparent; margin-top: 20px; margin-left : 20px")
-
-            # add to layout
-            layout_mobil = QHBoxLayout(tiket_mobil)
-            layout_mobil.setContentsMargins(0, 0, 0, 0)
-            layout_mobil.addWidget(radio_mobil)
-            layout_mobil.addWidget(img_mobil)
-            layout_mobil.addWidget(label_mobil)
-            layout_mobil.setAlignment(Qt.AlignCenter)
-
-            tiket_pesawat = QLabel()
-            tiket_pesawat.setContentsMargins(0, 0, 0, 0)
-            tiket_pesawat.setMaximumWidth(600)
-            tiket_pesawat.setMaximumHeight(200)
-
-            label_pesawat = QLabel("Tiket Pesawat Rp. " + str(arrray_of_destiansi[i][3]))
-            label_pesawat.setStyleSheet("color: #000000; font-size: 32px; font-weight: bold; margin-bottom : 20px; margin-top : 20px; ")
-            # set coordinate
-            label_pesawat.setContentsMargins(0, 0, 0, 0)
-
-            # add an image
-            img_pesawat = QLabel()
-            img_pesawat.setStyleSheet("background-color: #000000; border-radius: 10px; margin-bottom : 20px")
-            img_pesawat.setPixmap(QPixmap("D:/Semester IV/RPL/2/if2250-2023-k03-01-kitabisajalanjalan/img/biayaTransport/Plane.png"))
-            img_pesawat.setStyleSheet("background-color : transparent;")
-            img_pesawat.setMaximumWidth(70)
-
-            # make a radio button
+            # radio_kereta.setStyleSheet("margin-top:150px")
             radio_pesawat = QRadioButton()
+            # radio_pesawat.setStyleSheet("margin-top:150px")
+            radio_mobil = QRadioButton()
+            # radio_mobil.setStyleSheet("margin-top:150px")
+            radio_bus = QRadioButton()
+            # radio_bus.setStyleSheet("margin-top:150px")
+
+            # make a group
+            group = QButtonGroup()
+            group.addButton(radio_kereta)
             group.addButton(radio_pesawat)
-            radio_pesawat.setStyleSheet("background-color : transparent; margin-top: 20px; margin-left : 58px")
+            group.addButton(radio_mobil)
+            group.addButton(radio_bus)
 
-            # add to layout
-            layout_pesawat = QHBoxLayout(tiket_pesawat)
-            layout_pesawat.setContentsMargins(0, 0, 0, 0)
-            layout_pesawat.addWidget(radio_pesawat)
-            layout_pesawat.addWidget(img_pesawat)
-            layout_pesawat.addWidget(label_pesawat)
-            layout_pesawat.setAlignment(Qt.AlignCenter)
+            radio_container_layout = QVBoxLayout(radio_container)
+            radio_container_layout.addWidget(radio_kereta)
+            radio_container_layout.addWidget(radio_pesawat)
+            radio_container_layout.addWidget(radio_mobil)
+            radio_container_layout.addWidget(radio_bus)
 
-            tiket_bus = QLabel()
-            # tiket_bus.setMargin(-100)
-            tiket_bus.setContentsMargins(0, 0, 0, 0)
-            tiket_bus.setMaximumWidth(620)
-            tiket_bus.setMaximumHeight(200)
+            radio_container_layout.setContentsMargins(30, 35, 0, 0)
+            # radio_container_layout.setStyleSheet("margin-right: 0")
+            radio_container_layout.setSpacing(60)
+            right_box_layout.addWidget(radio_container)
 
+            # make an image container
+            img_container = QLabel()
+            img_container.setFixedWidth(100)
+            # img_container.setFixedSize(800, 200)
+            img_container.setStyleSheet("margin : 0")
+
+            img_pesawat = QLabel()
+            img_pesawat.setPixmap(QPixmap("../img/biayaTransport/Plane.png"))
+            img_kereta = QLabel()
+            img_kereta.setPixmap(QPixmap("../img/biayaTransport/Train.png"))
+            img_mobil = QLabel()
+            img_mobil.setPixmap(QPixmap("../img/biayaTransport/People in Car.png"))
+            img_bus = QLabel()
+            img_bus.setPixmap(QPixmap("../img/biayaTransport/Bus.png"))
+
+            img_container_layout = QVBoxLayout(img_container)
+            img_container_layout.addWidget(img_kereta)
+            img_container_layout.addWidget(img_pesawat)
+            img_container_layout.addWidget(img_mobil)
+            img_container_layout.addWidget(img_bus)
+            img_container_layout.setAlignment(Qt.AlignLeft)
+
+            img_container_layout.setContentsMargins(0, 0, 0, 0)
+            img_container_layout.setSpacing(20)
+            right_box_layout.addWidget(img_container)
+
+            # make a label container
+            label_container = QLabel()
+            label_container_layout = QVBoxLayout(label_container)
+            label_container_layout.setContentsMargins(0, 0, 0, 0)
+            label_container_layout.setSpacing(20)
+            
+            # make a label
+            label_kereta = QLabel("Tiket Kereta Rp. " + str(arrray_of_destiansi[i][1]))
+            label_kereta.setStyleSheet("color: #000000; font-size: 32px; font-weight: bold; margin-bottom : 20px; margin-top : 20px; ")
+            label_pesawat = QLabel("Tiket Pesawat Rp. " + str(arrray_of_destiansi[i][2]))
+            label_pesawat.setStyleSheet("color: #000000; font-size: 32px; font-weight: bold; margin-bottom : 20px; margin-top : 20px; ")
+            label_mobil = QLabel("Tiket Mobil Rp. " + str(arrray_of_destiansi[i][3]))
+            label_mobil.setStyleSheet("color: #000000; font-size: 32px; font-weight: bold; margin-bottom : 20px; margin-top : 20px; ")
             label_bus = QLabel("Tiket Bus Rp. " + str(arrray_of_destiansi[i][4]))
             label_bus.setStyleSheet("color: #000000; font-size: 32px; font-weight: bold; margin-bottom : 20px; margin-top : 20px; ")
-            # set coordinate
-            label_bus.setContentsMargins(0, 0, 0, 0)
 
-            # add an image
-            img_bus = QLabel()
-            img_bus.setStyleSheet("background-color: #000000; border-radius: 10px; margin-bottom : 20px")
-            img_bus.setPixmap(QPixmap("D:/Semester IV/RPL/2/if2250-2023-k03-01-kitabisajalanjalan/img/biayaTransport/Bus.png"))
-            img_bus.setStyleSheet("background-color : transparent;")
-            img_bus.setMaximumWidth(120)
-
-            # make a radio button
-            radio_bus = QRadioButton()
-            group.addButton(radio_bus)
-            radio_bus.setStyleSheet("background-color : transparent; margin-top: 20px;")
-
-            # add to layout
-            layout_bus = QHBoxLayout(tiket_bus)
-            layout_bus.setContentsMargins(0, 0, 0, 0)
-            layout_bus.addWidget(radio_bus)
-            layout_bus.addWidget(img_bus)
-            layout_bus.addWidget(label_bus)
-            layout_bus.setAlignment(Qt.AlignCenter)
-
-            right_box_layout.addWidget(tiket_kereta)
-            right_box_layout.addWidget(tiket_mobil)
-            right_box_layout.addWidget(tiket_pesawat)
-            right_box_layout.addWidget(tiket_bus)
+            label_container_layout.addWidget(label_kereta)
+            label_container_layout.addWidget(label_pesawat)
+            label_container_layout.addWidget(label_mobil)
+            label_container_layout.addWidget(label_bus)
+            label_container_layout.setAlignment(Qt.AlignLeft)
+            right_box_layout.addWidget(label_container)
+            right_box_layout.setSpacing(0)
             right_box_layout.setContentsMargins(0, 0, 0, 0)
-            # img_kereta.move(0, 0)
-
-            # right_box.setText("Harga Tiket Kereta : "+str(arrray_of_destiansi[i][1])+"\nHarga Tiket Mobil : "+str(arrray_of_destiansi[i][2])+"\nHarga Tiket Pesawat : "+str(arrray_of_destiansi[i][3])+"\nHarga Tiket Bus : "+str(arrray_of_destiansi[i][4]))
+            # right_box_layout.setAlignment(Qt.AlignLeft)
+            # right_box_layout.setAlignment(Qt.AlignCenter)
+            # right_box_layout.setSpacing(0)
             box.addWidget(right_box)
 
             self.gridLayout.addWidget(self.container, i, 0, 1, 1)
