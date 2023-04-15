5496893e61d4674a0feeb5523387a62b3e78bbf8
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-04-15 20:51:07 +0700

    feat: description follows database

--- src/interface/pilihDestinasi.py
+++ src/interface/pilihDestinasi.py
@@ -64,7 +64,7 @@ class pilihDestinasiWindow(QDialog):
             scroll_area.setFrameShape(QtWidgets.QFrame.NoFrame)
             scroll_area.setStyleSheet("background-color: transparent;")
             
-            desc = QLabel('lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+            desc = QLabel(ListDestinasi[i][4])
             # set font size to 14 with font inter
             desc.setStyleSheet("font-size: 14px; font-family: Inter; color: #000000;")
             # desc.setFixedHeight(100)
