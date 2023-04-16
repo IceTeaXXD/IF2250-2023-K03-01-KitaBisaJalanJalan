01503cbab66eb0c897c149bbbecfa06c27c728c7
goodgirlwannabe <13521006@mahasiswa.itb.ac.id>
2023-04-16 18:43:01 +0700

    feat: add TestBoundaryDaerahWisata

---
+++ test/TestBoundaryDaerahWisata.py
@@ -0,0 +1,25 @@
+import unittest
+import os
+from ..src.classes.BoundaryDaerahWisata import *
+
+class Test(unittest.TestCase):
+    def __init__(self, methodName: str) -> None:
+        super().__init__(methodName=methodName)
+        self.listDaerahWisata = BoundaryDaerahWisata.getListDaerah()
+    
+    def test_getListDaerah(self):
+        self.assertEqual(len(self.listDaerahWisata), 3)
+
+    def test_getDaerah(self):
+        self.assertEqual(self.listDaerahWisata[0].getID(), 1)
+        self.assertEqual(self.listDaerahWisata[0].getNama(), "Jakarta")
+        self.assertEqual(self.listDaerahWisata[9].getID(), 2)
+        self.assertEqual(self.listDaerahWisata[9].getNama(), "Bandung")
+    
+    def test_printAll(self):
+        self.assertEqual(self.listDaerahWisata[0].print(), "1. Jakarta")
+        self.assertEqual(self.listDaerahWisata[9].print(), "2. Bandung")
+
+
+if __name__ == "__main__":
+    unittest.main()
