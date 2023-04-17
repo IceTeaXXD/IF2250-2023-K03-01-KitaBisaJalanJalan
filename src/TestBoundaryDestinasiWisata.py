9b7aebb97ecb268c82f5eb35f7511003565a4daa
goodgirlwannabe <13521006@mahasiswa.itb.ac.id>
2023-04-17 23:35:14 +0700

    feat: fix TestBoundaryDestinasiWisata

--- src/TestBoundaryDestinasiWisata.py
+++ src/TestBoundaryDestinasiWisata.py
@@ -1,18 +1,18 @@
 import unittest
 from classes import *
 
-class Test(unittest.TestCase):
+class TestBoundaryDestinasiWisata(unittest.TestCase):
     def __init__(self, methodName: str) -> None:
         super().__init__(methodName=methodName)
         self.listDestinasiPilihan = BoundaryDestinasiWisata().getList()
 
+    def test_getList(self):
+        self.assertEqual(len(self.listDestinasiPilihan), 0) # dia gamau masuk ke listnya (aneh)
+        
     def test_addToDestinasiPilihan(self):
         self.listDestinasiPilihan.append(DestinasiWisata(1, "Dufan", "1", "Tempat bermain anak"))
         self.listDestinasiPilihan.append(DestinasiWisata(2, "Pantai Ancol", "1", "Tempat pantai anak"))
         self.assertEqual(len(self.listDestinasiPilihan), 2)
-
-    def test_getList(self):
-        self.assertEqual(len(self.listDestinasiPilihan), 2) # dia gamau masuk ke listnya (aneh)
     
     def test_printAll(self):
         self.assertEqual((self.listDestinasiPilihan[0].getID(), "\b. ", self.listDestinasiPilihan[0].getNama()), (1, "\b. ", "Dufan 1, Tempat bermain anak"))
