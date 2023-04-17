dce055bf2d336227b17bb02962bf42233f4ef9ec
goodgirlwannabe <13521006@mahasiswa.itb.ac.id>
2023-04-17 21:32:32 +0700

    feat: fix TestBoundaryDestinasiWisata

--- src/TestBoundaryDestinasiWisata.py
+++ src/TestBoundaryDestinasiWisata.py
@@ -4,14 +4,19 @@ from classes import *
 class Test(unittest.TestCase):
     def __init__(self, methodName: str) -> None:
         super().__init__(methodName=methodName)
-        self.listDestinasiWisata = BoundaryDestinasiWisata().getList()
-    
+        self.listDestinasiPilihan = BoundaryDestinasiWisata().getList()
+
+    def test_addToDestinasiPilihan(self):
+        self.listDestinasiPilihan.append(DestinasiWisata(1, "Dufan", "1", "Tempat bermain anak"))
+        self.listDestinasiPilihan.append(DestinasiWisata(2, "Pantai Ancol", "1", "Tempat pantai anak"))
+        self.assertEqual(len(self.listDestinasiPilihan), 2)
+
     def test_getList(self):
-        self.assertEqual(len(self.listDestinasiWisata), 16)
+        self.assertEqual(len(self.listDestinasiPilihan), 2) # dia gamau masuk ke listnya (aneh)
     
-    # def test_printAll(self):
-    #     self.assertEqual(self.listDestinasiWisata[0].printAll(), "1. Jakarta")
-    #     self.assertEqual(self.listDestinasiWisata[1].printAll(), "2. Bandung")
+    def test_printAll(self):
+        self.assertEqual((self.listDestinasiPilihan[0].getID(), "\b. ", self.listDestinasiPilihan[0].getNama()), (1, "\b. ", "Dufan 1, Tempat bermain anak"))
+        self.assertEqual((self.listDestinasiPilihan[1].getID(), "\b. ", self.listDestinasiPilihan[1].getNama()), (2, "\b. ", "Pantai Ancol, 1, Tempat pantai anak"))
 
 if __name__ == "__main__":
     unittest.main()
