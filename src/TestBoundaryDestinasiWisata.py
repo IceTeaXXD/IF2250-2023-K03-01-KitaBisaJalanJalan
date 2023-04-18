520c8845d3718e77691c62bc037e0e996f66b8ec
goodgirlwannabe <13521006@mahasiswa.itb.ac.id>
2023-04-19 02:52:31 +0700

    feat: fix TestBoundaryDestinasiWisata

--- src/TestBoundaryDestinasiWisata.py
+++ src/TestBoundaryDestinasiWisata.py
@@ -7,16 +7,19 @@ class TestBoundaryDestinasiWisata(unittest.TestCase):
         self.listDestinasiPilihan = BoundaryDestinasiWisata().getList()
 
     def test_getList(self):
-        self.assertEqual(len(self.listDestinasiPilihan), 0) # dia gamau masuk ke listnya (aneh)
+        BoundaryDestinasiWisata.addToDestinasiPilihan(self, DestinasiWisata(1, "Dufan", "1", "Tempat bermain anak"))
+        self.assertEqual(len(BoundaryDestinasiWisata.getList(self)), 1) 
         
     def test_addToDestinasiPilihan(self):
-        self.listDestinasiPilihan.append(DestinasiWisata(1, "Dufan", "1", "Tempat bermain anak"))
-        self.listDestinasiPilihan.append(DestinasiWisata(2, "Pantai Ancol", "1", "Tempat pantai anak"))
+        BoundaryDestinasiWisata.addToDestinasiPilihan(self, DestinasiWisata(1, "Dufan", "1", "Tempat bermain anak"))
+        BoundaryDestinasiWisata.addToDestinasiPilihan(self, DestinasiWisata(2, "Pantai Ancol", "1", "Tempat pantai anak"))
         self.assertEqual(len(self.listDestinasiPilihan), 2)
     
     def test_printAll(self):
-        self.assertEqual((self.listDestinasiPilihan[0].getID(), "\b. ", self.listDestinasiPilihan[0].getNama()), (1, "\b. ", "Dufan 1, Tempat bermain anak"))
-        self.assertEqual((self.listDestinasiPilihan[1].getID(), "\b. ", self.listDestinasiPilihan[1].getNama()), (2, "\b. ", "Pantai Ancol, 1, Tempat pantai anak"))
+        BoundaryDestinasiWisata.addToDestinasiPilihan(self, DestinasiWisata(1, "Dufan", "1", "Tempat bermain anak"))
+        BoundaryDestinasiWisata.addToDestinasiPilihan(self, DestinasiWisata(2, "Pantai Ancol", "1", "Tempat pantai anak"))
+        self.assertEqual((self.listDestinasiPilihan[0].getID(), "\b.", self.listDestinasiPilihan[0].getNamaDestinasi()), (1, "\b.", "Dufan"))
+        self.assertEqual((self.listDestinasiPilihan[1].getID(), "\b.", self.listDestinasiPilihan[1].getNamaDestinasi()), (2, "\b.", "Pantai Ancol"))
 
 if __name__ == "__main__":
     unittest.main()
