98736aa5831c4a3c5af008c3748dcae4b0ca3872
goodgirlwannabe <13521006@mahasiswa.itb.ac.id>
2023-04-19 02:42:25 +0700

    feat: fix TestBoundaryListTransportasi

--- src/TestBoundaryListTransportasi.py
+++ src/TestBoundaryListTransportasi.py
@@ -8,16 +8,16 @@ class TestBoundaryListTransportasi(unittest.TestCase):
     
     def test_addToPerjalanan(self):
         self.assertEqual(len(self.listTransportasiPilihan), 0)
-        self.listTransportasiPilihan.append(metodeTransportasi(1, "Kereta", 50000))
+        BoundaryListTransportasi.addToPerjalanan(self, metodeTransportasi(1, "Kereta", 50000))
         self.assertEqual(len(self.listTransportasiPilihan), 1)
     
     def test_getList(self):
-        self.assertEqual(len(self.listTransportasiPilihan), 1) #gangerti dia ttp kosong
+        BoundaryListTransportasi.addToPerjalanan(self, metodeTransportasi(1, "Kereta", 50000))
+        self.assertEqual(len(BoundaryListTransportasi.getList(self)), 1)
 
     def test_printAll(self):
-        self.assertEqual(self.listTransportasiPilihan[0].getID(), 1)
-        self.assertEqual(self.listTransportasiPilihan[0].getNama(), "Kereta")
-        self.assertEqual(self.listTransportasiPilihan[0].getHarga(), 50000)
+        BoundaryListTransportasi.addToPerjalanan(self, metodeTransportasi(1, "Kereta", 50000))
+        self.assertEqual((self.listTransportasiPilihan[0].getID(), "\b. ", self.listTransportasiPilihan[0].getNama(), "\b", self.listTransportasiPilihan[0].getHarga()), (1, "\b. ", "Kereta", "\b", 50000))
 
 if __name__ == "__main__":
     unittest.main()
