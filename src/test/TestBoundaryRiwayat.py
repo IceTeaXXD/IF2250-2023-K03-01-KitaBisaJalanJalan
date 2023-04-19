a535c128699a51639782521ceb1269ebf0c65037
MHEN2606 <matthew.mahendra@gmail.com>
2023-04-19 20:40:20 +0700

    fix: test unit

--- src/test/TestBoundaryRiwayat.py
+++ src/test/TestBoundaryRiwayat.py
@@ -1,14 +1,25 @@
 import unittest
+import sqlite3
 from classes import *
 
 class TestBoundaryRiwayat(unittest.TestCase):
     def test_getRiwayat(self):
         listRiwayat = BoundaryRiwayat().getRiwayat()
-        self.assertEqual(len(listRiwayat), 0) # gak tau masi bingung
+        query = "select * from riwayatperjalanan where DATE('now') between tgl_mulai and tgl_akhir"
+        conn = sqlite3.connect('./database/kitabisajalan.db')
+        c = conn.cursor()
+        c.execute(query)
+        self.assertEqual(len(listRiwayat), len(c.fetchall())) # gak tau masi bingung
+        c.close()
 
     def test_getRiwayatBerlangsung(self):
         listRiwayatBerlangsung = BoundaryRiwayat().getRiwayatBerlangsung()
-        self.assertEqual(len(listRiwayatBerlangsung), 0) # ini juga gak tau
+        query = "select * from riwayatperjalanan where DATE('now') between tgl_mulai and tgl_akhir"
+        conn = sqlite3.connect('./database/kitabisajalan.db')
+        c = conn.cursor()
+        c.execute(query)
+        self.assertEqual(len(listRiwayatBerlangsung), len(c.fetchall())) # ini juga gak tau
+        c.close()
 
 
 if __name__ == "__main__":
